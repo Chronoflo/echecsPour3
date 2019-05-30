c080dae9f1c857702fa50321a64ee86276daaa82
Florian <florian.c.delprat@gmail.com>
2019-05-30 17:27:21 +0200

    Pan.

--- Echec à 3.py
+++ Echec à 3.py
@@ -1,1 +1,17 @@
 ﻿# Jeu d'Echec pour 3 joueurs
+class ArmesAFeu:
+    def __init__(self, nom, munitions, possede_viseur):
+        self.nom = nom
+        self.munitions = munitions
+        self.possede_viseur = possede_viseur
+
+    def tire(self):
+        if self.munitions > 0:
+            print("Pan !")
+        else:
+            print("Ma vie est un échec.")
+
+
+class Pistolet(ArmesAFeu):
+    def __init__(self, munitions):
+        super().__init__("Piou piou", munitions, False)
