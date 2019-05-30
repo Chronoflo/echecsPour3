d122428d64923bc2471cf4a9f43fd04c110f5aee
Shampook <mage.du.web@gmail.com>
2019-05-30 19:21:24 +0200

    initialisation du plateau.

--- Echec à 3.py
+++ Echec à 3.py
@@ -42,3 +42,12 @@ class ListesDeJoueur:
             if iJoueurAEnlever <= self.iJoueurActuel:
                 self.iJoueurActuel -= 1
             del self.joueurs[iJoueurAEnlever]
+
+def initialisation_plateau():
+    t=[[[Roi],  [Tour2],    [Fou2],     [Pion7],[None],[None]],
+       [[Fou1], [Reine],    [Cavalier2],[Pion6],[None],[None]]
+       [[Tour1],[Cavalier1],[Chèvre],   [Pion5],[None],[None]]
+       [[Pion1],[Pion2],    [Pion3],    [Pion4],[None],[None]]
+       [[None], [None],     [None],     [None], [None],[None]]
+       [[None], [None],     [None],     [None], [None],[None]]]
+    return [t for i in range(3)]
