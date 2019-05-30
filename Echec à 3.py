42881d6e065067db6945a5a911c9d346c01f66a8
Florian <florian.c.delprat@gmail.com>
2019-05-30 19:11:17 +0200

    Création classe ListeDeJoueurs et réorganisation du projet.

--- Echec à 3.py
+++ Echec à 3.py
@@ -1,9 +1,11 @@
-﻿# Jeu d'Echec pour 3 joueurs
+﻿from constantes import *
+
+# Jeu d'Echec pour 3 joueurs
 class ArmesAFeu:
-    def __init__(self, nom, munitions, possede_viseur):
+    def __init__(self, nom, munitions, possedeViseur):
         self.nom = nom
         self.munitions = munitions
-        self.possede_viseur = possede_viseur
+        self.possede_viseur = possedeViseur
 
     def tire(self):
         if self.munitions > 0:
@@ -15,3 +17,28 @@ class ArmesAFeu:
 class Pistolet(ArmesAFeu):
     def __init__(self, munitions):
         super().__init__("Piou piou", munitions, False)
+
+
+class ListesDeJoueur:
+    def __init__(self):
+        self.joueurs = [J1, J2, J3]
+        self.iJoueurActuel = 0
+
+    def joueur_suivant(self):
+        if self.iJoueurActuel + 1 < len(self.joueurs):
+            self.iJoueurActuel += 1
+        else:
+            self.iJoueurActuel = 0
+
+        return self.joueurs[self.iJoueurActuel]
+
+    def enleve_joueur(self, joueur):
+        try:
+            iJoueurAEnlever = self.joueurs.index(joueur)
+        except ValueError:
+            print("Ce joueur ne joue pas.")
+            return None
+        else:
+            if iJoueurAEnlever <= self.iJoueurActuel:
+                self.iJoueurActuel -= 1
+            del self.joueurs[iJoueurAEnlever]
