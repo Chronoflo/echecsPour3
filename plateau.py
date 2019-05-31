7355f3dcf82adef8c13ca281b738d1b943d112e9
Florian <florian.c.delprat@gmail.com>
2019-05-31 12:34:46 +0200

    Modifications joueur et plateau

--- plateau.py
+++ plateau.py
@@ -7,11 +7,20 @@ def initialisation_plateau(listeJoueurs):
     """ Retourne un plateau pour len(listeJoueurs) joueurs. """
     return [
         [
-            [[joueur.Roi],   [joueur.Tour2],     [joueur. Fou2],     [joueur.Pion7], [None], [None]],
-            [[joueur.Fou1],  [joueur.Reine],     [joueur.Cavalier2], [joueur.Pion6], [None], [None]],
-            [[joueur.Tour1], [joueur.Cavalier1], [joueur.Chevre],    [joueur.Pion5], [None], [None]],
-            [[joueur.Pion1], [joueur.Pion2],     [joueur.Pion3],     [joueur.Pion4], [None], [None]],
-            [[None],         [None],             [None],             [None],         [None], [None]],
-            [[None],         [None],             [None],             [None],         [None], [None]]
+            [joueur.roi,   joueur.tour2,     joueur. fou2,     joueur.pion7, None, None],
+            [joueur.fou1,  joueur.reine,     joueur.cavalier2, joueur.pion6, None, None],
+            [joueur.tour1, joueur.cavalier1, joueur.chevre,    joueur.pion5, None, None],
+            [joueur.pion1, joueur.pion2,     joueur.pion3,     joueur.pion4, None, None],
+            [None,         None,             None,             None,         None, None],
+            [None,         None,             None,             None,         None, None]
         ]
         for joueur in listeJoueurs]
+
+
+from joueur import *
+
+
+joueurs = [Joueur("Arhur", 2), Joueur("Sarah", 3), Joueur("Florian", 4)]
+
+plateau = Plateau(joueurs)
+print(plateau[0][0][0].nomJoueur)
