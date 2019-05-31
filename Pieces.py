fc3336f2595dcf0574dcc1e2fb0b7db28f495733
Artoxus <arthurfiquet@hotmail.fr>
2019-05-31 12:12:53 +0200

    Classe toutes les pieces + cte

--- Pieces.py
+++ Pieces.py
@@ -14,36 +14,24 @@ class Pion(Piece):
         super().__init__("Pion")
 
     def deplacementsPossibles(self, emplacementPresent):
-        """ Envois un Booléen devant qui dit si le déplacement est infini ou
+        """ Envois un Booléen (0,1,2 pour respectivement False,True,Rock)
+        devant qui dit si le déplacement est infini ou
         fini, le tableau des déplacements fini (vecteur des déplacements
         infinis) possible du Pion.
         Separe le cas où il y a une piece ennemie et où il n'y a rien.
         Avec à la fin le max des déplacements possibles si déplacement infini"""
-#
-        """tabAvecEnnemies = [[emplacementPresent[0],emplacementPresent[1]+1,
-                        emplacementPresent[2]+1],
-                        [emplacementPresent[0],emplacementPresent[1]+1,
-                        emplacementPresent[2]-1],
-                        [emplacementPresent[0],emplacementPresent[1]-1,
-                        emplacementPresent[2]+1]]
+
+        tabAvecEnnemis = 0, [ (1,1), (-1,1), (1,-1) ]
+        """ La régle de la prise en passant n'est pas présente dans cette
+        version """
 
         if self.emplacementInitial:
-            tabSansEnnemies = [[emplacementPresent[0],emplacementPresent[1]+1,
-                        emplacementPresent[2]],
-                        [emplacementPresent[0],emplacementPresent[1],
-                        emplacementPresent[2]+1]
-                        [emplacementPresent[0],emplacementPresent[1]+2,
-                        emplacementPresent[2]]
-                        [emplacementPresent[0],emplacementPresent[1],
-                        emplacementPresent[2]+2]]
+            tabSansEnnemis = 1 , [ (1,0), (0,1) ], 2
 
         else:
-            tabSansEnnemies = [[emplacementPresent[0],emplacementPresent[1]+1,
-                        emplacementPresent[2]],
-                        [emplacementPresent[0],emplacementPresent[1],
-                        emplacementPresent[2]+1]]
+            tabSansEnnemis = 0 , [ (1,0), (0,1) ]
 
-        return (False, tabAvecEnnemies, tabSansEnnemies)"""
+        return (tabSansEnnemis, tabAvecEnnemis)
 
 class Roi(Piece):
     """ Definit le Roi, deplacements autorisés etc... """
@@ -52,26 +40,15 @@ class Roi(Piece):
 
     def deplacementsPossibles(self, emplacementPresent):
         """ Envois le tableau des déplacement possible du Roi
-        Avec un Booléen devant qui dit si le déplacement est infini ou fini """
-#
-        """tab = [[emplacementPresent[0],emplacementPresent[1]+1,
-                   emplacementPresent[2]],
-               [emplacementPresent[0],emplacementPresent[1],
-                   emplacementPresent[2]+1]
-               [emplacementPresent[0],emplacementPresent[1]-1,
-                   emplacementPresent[2]]
-               [emplacementPresent[0],emplacementPresent[1],
-                   emplacementPresent[2]-1]
-               [emplacementPresent[0],emplacementPresent[1]+1,
-                   emplacementPresent[2]]+1,
-               [emplacementPresent[0],emplacementPresent[1]-1,
-                   emplacementPresent[2]+1]
-               [emplacementPresent[0],emplacementPresent[1]+1,
-                   emplacementPresent[2]-1]
-               [emplacementPresent[0],emplacementPresent[1]-1,
-                   emplacementPresent[2]-1]]
+        Avec la possiblilité de rock"""
 
-    return (False, tab, tab)"""
+        tabSansEnnemis = (0 , [(0,1),(1,0),(-1,0),(0,-1), (1,1),(1,-1),
+                            (-1,1),(-1,-1)] ), (2 , [(2,0), (1,0)] )
+
+        tabAvecEnnemis = 0 , [(0,1),(1,0),(-1,0),(0,-1),
+                             (1,1),(1,-1), (-1,1),(-1,-1)]
+
+        return (tabSansEnnemis, tabAvecEnnemis)
 
 class Cavalier(Piece):
     """ Definit le Roi, deplacements autorisés etc... """
@@ -79,8 +56,58 @@ class Cavalier(Piece):
         super().__init__("Cavalier")
 
     def deplacementsPossibles(self, emplacementPresent):
-        """ Envois le tableau des déplacement possible de la Cavalier """
+        """ Envois le tableau des déplacement possible du Cavalier """
 
+        tab = 0, [(2,1), (1,2), (-2,1), (-1,2),
+                     (2,-1), (1,-2), (-2,-1), (-1,-2)]
 
+        return (tab, tab)
 
+class Chevre(Piece):
+    """ Definit la Chevre, deplacements autorisés etc... (nouvelle piece) """
+    def __init__(self):
+        super().__init__("Chevre")
 
+    def deplacementsPossibles(self, emplacementPresent):
+        """ Envois le tableau des déplacement possible de la Chevre """
+
+        tab = 0 , [(2,0), (0,2), (-2,0), (0,-2),
+                  (2,2), (-2,-2), (-2,2), (2,-2)]
+
+        return (tab, tab)
+
+class Tour(Piece):
+    """ Definit la Tour, deplacements autorisés etc... """
+    def __init__(self):
+        super().__init__("Chevre")
+
+    def deplacementsPossibles(self, emplacementPresent):
+        """ Envois le tableau des déplacement possible de la Tour """
+
+        tab = 1 , [(1,0), (0,1), (-1,0), (0,-1)]
+
+        return (tab, tab)
+
+class Fou(Piece):
+    """ Definit le Fou, deplacements autorisés etc... """
+    def __init__(self):
+        super().__init__("Fou")
+
+    def deplacementsPossibles(self, emplacementPresent):
+        """ Envois le tableau des déplacement possible du Fou"""
+
+        tab = 1 , [(1,1), (1,-1), (-1,1), (-1,-1)]
+
+        return (tab, tab)
+
+class Reine(Piece):
+    """ Definit la Reine, deplacements autorisés etc... """
+    def __init__(self):
+        super().__init__("Reine")
+
+    def deplacementsPossibles(self, emplacementPresent):
+        """ Envois le tableau des déplacement possible de la Reine"""
+
+        tab = 1 , [(1,0), (0,1), (-1,0), (0,-1), (1,1), (1,-1), (-1,1), (-1,-1)]
+
+        return (tab, tab)
--- constantes.py
+++ constantes.py
@@ -1,3 +1,7 @@
 J1 = 0
 J2 = 1
 J3 = 2
+
+DeplacementFini = 0
+DeplacementInfini = 1
+Rock = 2
