98a34c9f2c2f248dc6baf3057e99537dde200396
Florian <florian.c.delprat@gmail.com>
2019-06-03 00:34:52 +0200

    Déplacement de la fonction dessine_composants

--- Interface.py
+++ Interface.py
@@ -1,9 +1,5 @@
-from math import sqrt, pi
-from numpy import exp
-
 import pygame
 from pygame.locals import *
-from Pieces import Piece
 
 
 class SuperColor(pygame.Color):
@@ -18,27 +14,6 @@ VERT = SuperColor("vert", (0, 255,   0))
 BLEU = SuperColor("bleu", (0,   0, 255))
 BLANC = SuperColor("blanc", (255, 255, 255))
 NOIR = SuperColor("noir", (0, 0, 0))
-
-
-def dessine_composants(surface, plateau):
-    """ Affiche tous les composants du plateau. """
-    def cercle(col, pos, radius=2):
-        pygame.draw.circle(surface, col, pos, radius)
-
-    center = complex(400, 400)  # Centre du plateau
-    c = 30  # Taille de la diagonale la plus courte d'une case
-    cercle(VERT, (int(center.real), int(center.imag)), 8)
-    for p in range(3):
-        origine = (center + c * 6 * exp((pi/2 + 2*p*pi/3) * 1j))  # Point (0,0) pour le terrain p
-        for d in range(6):
-            for g in range(6):
-                if isinstance(plateau[p][d][g], Piece):
-                    # Calcule la position d'une pièce
-                    z = origine - c*((d - g) * (sqrt(3)/2) + (1 + d + g) * 1j/2) * exp(p*2j*pi/3)
-                    # Affiche la pièce
-                    image = plateau[p][d][g].image
-                    x, y, w, h = image.get_rect()
-                    surface.blit(plateau[p][d][g].image, (z.real - w/2, z.imag - h/2))
 
 
 def affichage():
