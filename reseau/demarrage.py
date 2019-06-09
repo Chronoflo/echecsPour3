81a547ce1af52b46bad9f0478c1ae895ee3f1c12
Florian <florian.c.delprat@gmail.com>
2019-06-09 16:34:39 +0200

    La création de profile est maintenant disponible

--- reseau/demarrage.py
+++ reseau/demarrage.py
@@ -1,7 +1,9 @@
 import os
 
 from kivy.config import Config
-
+from kivy.metrics import dp
+from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 from reseau import Serveur, Client
 
 Config.set('input', 'mouse', 'mouse,disable_multitouch')
@@ -9,14 +11,17 @@ from kivy.app import App
 from kivy.config import ConfigParser
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.properties import NumericProperty, ReferenceListProperty, Clock, ObjectProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, Clock, ObjectProperty, ListProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
-from kivy.uix.settings import Settings, SettingsWithSidebar
+from kivy.uix.settings import Settings, SettingsWithSidebar, SettingItem, SettingSpacer, SettingOptions
+import kivy.uix.settings
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
+
+kivy.uix.settings.__all__ += tuple("SettingSuperOptions")
 
 with open('kv/demarrage.kv', encoding='utf-8') as f:
     Builder.load_string(f.read())
@@ -104,7 +109,7 @@ class SuperSettings(Popup):
     def __init__(self):
         super(SuperSettings, self).__init__()
         self.s = SettingsWithSidebar()
-        
+
     def build(self):
         self.add_widget(self.s)
         return self
@@ -113,8 +118,86 @@ class SuperSettings(Popup):
         self.s.add_json_panel(title, config, data=data)
 
 
+class SettingSuperOptions(SettingOptions):
+    addPopup = ObjectProperty(None, allownone=True)
+    hasNewOption = BooleanProperty(False)
+
+    def _create_add_popup(self, instance):
+        content = BoxLayout(orientation='vertical', spacing='5dp')
+        popup_width = min(0.80 * Window.width, dp(300))
+        self.addPopup = popup = Popup(
+            content=content, title='Ajouter', size_hint=(None, None), size=(popup_width, '95dp'))
+        textInput = TextInput(multiline=False, hint_text='nom')
+
+        def on_validate():
+            newProfileOptions = App.get_running_app().newProfileOptions
+            if textInput.text:
+                self.options.append(textInput.text)
+                self.value = textInput.text
+                newProfileOptions.append(textInput.text)
+                self._update_popup()
+                popup.dismiss()
+        textInput.on_text_validate = on_validate
+        textInput.height = textInput.minimum_height
+        content.add_widget(textInput)
+
+        popup.open()
+        textInput.focus = True
+
+    def _create_popup(self, instance):
+        # crée la popup
+        content = BoxLayout(orientation='vertical', spacing='5dp')
+        popup_width = min(0.95 * Window.width, dp(500))
+        self.popup = popup = Popup(
+            content=content, title=self.title, size_hint=(None, None),
+            size=(popup_width, '400dp'))
+        popup.height = len(self.options) * dp(55) + dp(150)
+
+        # ajoute toutes les options
+        content.add_widget(Widget(size_hint_y=None, height=1))
+        uid = str(self.uid)
+        for option in self.options:
+            state = 'down' if option == self.value else 'normal'
+            btn = ToggleButton(text=option, state=state, group=uid)
+            btn.bind(on_release=self._set_option)
+            content.add_widget(btn)
+
+        content.add_widget(SettingSpacer())
+
+        add_btn = Button(text='Ajouter', size_hint_y=None, height=dp(50))
+        add_btn.bind(on_release=self._create_add_popup)
+        cancel_btn = Button(text='Annuler', size_hint_y=None, height=dp(50))
+        cancel_btn.bind(on_release=popup.dismiss)
+        content.add_widget(add_btn)
+        content.add_widget(cancel_btn)
+
+        # and open the popup !
+        popup.open()
+
+    def _update_popup(self):
+        content, popup = self.popup.content, self.popup
+
+        content.clear_widgets()
+        popup.height = len(self.options) * dp(55) + dp(150)
+        uid = str(self.uid)
+        for option in self.options:
+            state = 'down' if option == self.value else 'normal'
+            btn = ToggleButton(text=option, state=state, group=uid)
+            btn.bind(on_release=self._set_option)
+            content.add_widget(btn)
+
+        content.add_widget(SettingSpacer())
+        add_btn = Button(text='Ajouter', size_hint_y=None, height=dp(50))
+        add_btn.bind(on_release=self._create_add_popup)
+        cancel_btn = Button(text='Annuler', size_hint_y=None, height=dp(50))
+        cancel_btn.bind(on_release=self.popup.dismiss)
+        content.add_widget(add_btn)
+        content.add_widget(cancel_btn)
+
+
 class DemarrageApp(App):
     messagesHistoric = ObjectProperty(None)
+    newProfileOptions = ListProperty([])
 
     def __init__(self, lance_partie):
         super(DemarrageApp, self).__init__()
@@ -154,10 +237,15 @@ class DemarrageApp(App):
             config.read('default.ini')
 
     def build_settings(self, settings: Settings):
+        settings.register_type("superoptions", SettingSuperOptions)
         pages = ("Jeu", "Graphique", "Réseau", "Autre")
 
         for page in pages:
-            with open("json/{}.json".format(page), encoding='utf-8') as f:
+            file_path = "json/{}.json".format(page)
+            if not os.path.isfile(file_path):
+                file_path = "json/default_{}.json".format(page)
+
+            with open(file_path, encoding='utf-8') as f:
                 settings.add_json_panel(page, self.config, data=f.read())
 
     def open_settings(self, *largs):
@@ -179,6 +267,23 @@ class DemarrageApp(App):
         self.send("*{} s'est déconnecté*".format(self.config.get('gameplay', 'profile')))
         self.serveur.désactive()
         self.client.désactive()
+
+        if self.newProfileOptions:
+            res = ""
+            file_path = "json/Jeu.json"
+            if not os.path.isfile(file_path):
+                file_path = "json/default_Jeu.json"
+
+            with open(file_path, encoding='utf-8') as f:
+                for line in f:
+                    if line.strip()[:9] == '"options"':
+                        res += '        "options": {}],\n'.format(line[19:-3] + ', ' + ", ".join(['"' + name + '"' for name in self.newProfileOptions]))
+                    else:
+                        res += line
+            self.newProfileOptions = []
+
+            with open('json/Jeu.json', 'w', encoding='utf-8') as f:
+                f.write(res)
 
     def applique_parametres(self):
         if self.config.get('graphics', 'fullscreen') == '1':
